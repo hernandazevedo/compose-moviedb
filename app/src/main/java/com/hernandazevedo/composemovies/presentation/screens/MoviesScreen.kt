0427576438f799954e04931d27976508b3dda63c
Hernand Azevedo <hernand.azevedo@zup.com.br>
2025-03-02 18:54:43 -0300

    feat: add movie screen

--- app/src/main/java/com/hernandazevedo/composemovies/presentation/screens/MoviesScreen.kt
+++ app/src/main/java/com/hernandazevedo/composemovies/presentation/screens/MoviesScreen.kt
@@ -3,10 +3,12 @@ package com.hernandazevedo.composemovies.presentation.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,14 +43,21 @@ fun MoviesScreen(moviesList: PopularMoviesResult,
                  onClickNavigateToDetails: (Int) -> Unit,
                  onQueryChange: (String) -> Unit) {
     Column(modifier = Modifier.fillMaxSize()) {
-        val searchQuery by rememberSaveable {
+        var searchQuery by rememberSaveable {
             mutableStateOf("")
         }
         Spacer(modifier = Modifier.height(16.dp))
         SearchBar(
-
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(70.dp)
+                .clip(RoundedCornerShape(40.dp)),
             query = searchQuery,
-            onQueryChange = onQueryChange,
+            onQueryChange = { queryChanged ->
+                searchQuery = queryChanged
+                onQueryChange(queryChanged)
+            },
             onSearch = { _ ->
                 // Handle search ImeAction.Search here
             },
@@ -61,11 +71,6 @@ fun MoviesScreen(moviesList: PopularMoviesResult,
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = "Hello 1",
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis
-        )
         HeaderMoviesScreen(
             searchQuery = searchQuery,
             onClickNavigateToDetails = onClickNavigateToDetails,
@@ -168,8 +173,8 @@ fun MoviesScreenPrev() {
     val moviesTests = listOf(
         MovieDomain(
             id = 1,
-            title = "Ant-Man y la Avispa: Quantumanía",
-            overview = "La pareja de superhéroes Scott Lang y Hope van Dyne regresa para continuar sus aventuras como Ant-Man y la Avispa. Los dos, junto a los padres de Hope, Hank Pym y Janet van Dyne y la hija de Scott, Cassie Lang, se dedican a explorar el Mundo Cuántico, interactuando con nuevas y extrañas criaturas y embarcándose en una aventura que les llevará más allá de los límites de lo que creían posible.",
+            title = "Ant-Man and the Wasp: Quantumania",
+            overview = "Superhero duo Scott Lang and Hope van Dyne return to continue their adventures as Ant-Man and the Wasp. The two, along with Hope’s parents Hank Pym and Janet van Dyne and Scott’s daughter Cassie Lang, set out to explore the Quantum Realm, interacting with strange new creatures and embarking on an adventure that will take them beyond the limits of what they thought possible.",
             poster_path = "https://image.tmdb.org/t/p/original/lKHy0ntGPdQeFwvNq8gK1D0anEr.jpg",
             vote_average = 6.5f,
             release_date = "2022-02-17"
@@ -177,7 +182,7 @@ fun MoviesScreenPrev() {
         MovieDomain(
             id = 2,
             title = "Sisu",
-            overview = "En lo profundo de la naturaleza salvaje de Laponia, Aatami Korpi está buscando oro, pero después de tropezar con una patrulla nazi, comienza una persecución impresionante y hambrienta de oro a través de la naturaleza salvaje de Laponia destruida y minada.",
+            overview = "Deep in the wilderness of Lapland, Aatami Korpi is searching for gold, but after stumbling upon a Nazi patrol, he begins a breathtaking, gold-hungry chase through the destroyed and mined wilderness of Lapland.",
             poster_path = "https://image.tmdb.org/t/p/original/t9VXZkgcxpIwfPUKAWOOONs0vHv.jpg",
             vote_average = 7.4f,
             release_date = "2021-10-01"
